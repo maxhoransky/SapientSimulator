cd53e9f29200cc942f8a2bb353d3b18daf1322d4
maxhoransky <max.horansky@tutanota.com>
2022-11-04 19:08:58 +0100

    Fixed colours, added some groundwork for additional biome characteristics

--- Code/planet_lib.py
+++ Code/planet_lib.py
@@ -1,62 +1,27 @@
 #==============================================================================
 # Planet Max: Utility library file
 #------------------------------------------------------------------------------
-'''
-       0: {'color':'#4faed0', 'agrWork':  0, 'agrSource':    0, 'indWork':  0, 'indSource':    0},
-       
-     100: {'color':'#66ff33', 'agrWork':170, 'agrSource': 1800, 'indWork':110, 'indSource':  400},
-     200: {'color':'#5ceb2e', 'agrWork':180, 'agrSource': 2000, 'indWork':150, 'indSource':  450},
-     300: {'color':'#52d629', 'agrWork':185, 'agrSource': 1900, 'indWork':180, 'indSource':  500},
-     400: {'color':'#47c224', 'agrWork':175, 'agrSource': 1800, 'indWork':200, 'indSource':  600},
-     500: {'color':'#38a31c', 'agrWork':120, 'agrSource': 1300, 'indWork':210, 'indSource':  800},
-     600: {'color':'#298514', 'agrWork':100, 'agrSource': 1000, 'indWork':230, 'indSource': 1200},
-     700: {'color':'#1a660d', 'agrWork':100, 'agrSource':  900, 'indWork':250, 'indSource': 3000},
-     800: {'color':'#0a4705', 'agrWork':100, 'agrSource':  800, 'indWork':270, 'indSource': 4000},
-     900: {'color':'#003300', 'agrWork':100, 'agrSource':  750, 'indWork':300, 'indSource': 4500},
-     
-    1000: {'color':'#333300', 'agrWork':110, 'agrSource':  700, 'indWork':310, 'indSource': 4700},
-    1100: {'color':'#423300', 'agrWork':120, 'agrSource':  650, 'indWork':300, 'indSource': 4200},
-    1200: {'color':'#523300', 'agrWork':130, 'agrSource':  600, 'indWork':290, 'indSource': 3700},
-    1300: {'color':'#613300', 'agrWork':130, 'agrSource':  550, 'indWork':250, 'indSource': 2600},
-    1400: {'color':'#803300', 'agrWork':120, 'agrSource':  500, 'indWork':220, 'indSource': 1400},
-    1500: {'color':'#8f3300', 'agrWork':115, 'agrSource':  450, 'indWork':210, 'indSource': 1100},
-    1600: {'color':'#9e3300', 'agrWork':110, 'agrSource':  400, 'indWork':200, 'indSource':  900},
-    1700: {'color':'#ad3300', 'agrWork':100, 'agrSource':  370, 'indWork':190, 'indSource':  800},
-    1800: {'color':'#bd3300', 'agrWork': 90, 'agrSource':  330, 'indWork':180, 'indSource':  700},
-    1900: {'color':'#cc3300', 'agrWork': 95, 'agrSource':  300, 'indWork':170, 'indSource':  600},
-    
-    2000: {'color':'#d1470f', 'agrWork': 90, 'agrSource':  250, 'indWork':160, 'indSource':  500},
-    2100: {'color':'#d6591d', 'agrWork': 80, 'agrSource':  200, 'indWork':150, 'indSource':  450},
-    2200: {'color':'#da6c2b', 'agrWork': 50, 'agrSource':  150, 'indWork':140, 'indSource':  400},
-    2300: {'color':'#df7e38', 'agrWork': 20, 'agrSource':  100, 'indWork':130, 'indSource':  370},
-    2400: {'color':'#e8a354', 'agrWork': 20, 'agrSource':   50, 'indWork':120, 'indSource':  330},
-    2500: {'color':'#edb562', 'agrWork':  0, 'agrSource':    0, 'indWork':110, 'indSource':  300},
-    2600: {'color':'#f1c870', 'agrWork':  0, 'agrSource':    0, 'indWork':100, 'indSource':  250},
-    2700: {'color':'#f6da7d', 'agrWork':  0, 'agrSource':    0, 'indWork': 90, 'indSource':  200},
-    2800: {'color':'#faed8b', 'agrWork':  0, 'agrSource':    0, 'indWork': 90, 'indSource':  150},
-    2900: {'color':'#ffff99', 'agrWork':  0, 'agrSource':    0, 'indWork': 90, 'indSource':  100},
-    
-    3000: {'color':'#ffffff', 'agrWork':  0, 'agrSource':    0, 'indWork':  0, 'indSource':    0},
-    '''
+#add: forgaing+hunting, pastures,permissivity,
+# 'frgWork''frgSource''pstWork''pstSource''prms'
 #==============================================================================
 # Constans
 #------------------------------------------------------------------------------
 biomes = { 
-    "Sea":              {'color':'#2b53ff', 'agrWork':  0, 'agrSource':    0, 'indWork':  0, 'indSource':    0},
-    "Rainforest":       {'color':'#005f11', 'agrWork':  0, 'agrSource':    0, 'indWork':  0, 'indSource':    0},
-    "Monsoon":          {'color':'#008511', 'agrWork':170, 'agrSource': 1800, 'indWork':110, 'indSource':  400},
-    "Savannah":         {'color':'#8e852c', 'agrWork':180, 'agrSource': 2000, 'indWork':150, 'indSource':  450},
-    "Desert":           {'color':'#e4c518', 'agrWork':185, 'agrSource': 1900, 'indWork':180, 'indSource':  500},
-    "Cold desert":      {'color':'#C3D3EC', 'agrWork':175, 'agrSource': 1800, 'indWork':200, 'indSource':  600},
-    "Steppe":           {'color':'#91a35c', 'agrWork':120, 'agrSource': 1300, 'indWork':210, 'indSource':  800},
-    "Subtropical":      {'color':'#BA6120', 'agrWork':100, 'agrSource': 1000, 'indWork':230, 'indSource': 1200},
-    "Mediterranean":    {'color':'#D13A0F', 'agrWork':100, 'agrSource':  900, 'indWork':250, 'indSource': 3000},
-    "Marine":           {'color':'#86f4a2', 'agrWork':100, 'agrSource':  800, 'indWork':270, 'indSource': 4000},
-    "Humid":            {'color':'#25AB92', 'agrWork':100, 'agrSource':  750, 'indWork':300, 'indSource': 4500},
-    "Taiga":            {'color':'#9AB3AD', 'agrWork':110, 'agrSource':  700, 'indWork':310, 'indSource': 4700},
-    "Tundra":           {'color':'#B1C4C0', 'agrWork':120, 'agrSource':  650, 'indWork':300, 'indSource': 4200},
-    "Ice Caps":         {'color':'#bef9ff', 'agrWork':130, 'agrSource':  600, 'indWork':290, 'indSource': 3700},
-    "Mountains":        {'color':'#656565', 'agrWork':  0, 'agrSource':    0, 'indWork': 90, 'indSource':  100},
+    "Sea":              {'color':'#2b53ff', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Rainforest":       {'color':'#005f11', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Monsoon":          {'color':'#008511', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Savannah":         {'color':'#8e852c', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Desert":           {'color':'#fefc01', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Cold desert":      {'color':'#C3D3EC', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Steppe":           {'color':'#c4a53b', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Subtropical":      {'color':'#BA6120', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Mediterranean":    {'color':'#D13A0F', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Marine":           {'color':'#86f4a2', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Humid":            {'color':'#25AB92', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Taiga":            {'color':'#9AB3AD', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Tundra":           {'color':'#B1C4C0', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Ice Caps":         {'color':'#bef9ff', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
+    "Mountains":        {'color':'#656565', 'prmst': 0, 'agrWork':  0, 'agrSource':    0, 'frgWork':  0, 'frgSource':    0, 'pstrWork':  0, 'pstrSource':    0,'indWork':  0, 'indSource':    0},
 }
 
 #------------------------------------------------------------------------------
